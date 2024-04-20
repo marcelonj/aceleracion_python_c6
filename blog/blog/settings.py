28c3daa516068a391b208084f29466ed02c5eb57
Marcelo Jurado <marcelonjurado@gmail.com>
2024-04-19 21:02:00 -0300

    Se realizaron las configuraciones para media

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
